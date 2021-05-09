12cb741b13ff355ae2cdda5d3f7d9b751b398c4d
Christian Esteves <chan.esteves@gmail.com>
2021-05-09 20:39:01 +0800

    add haversine formula

--- app/Pair.php
+++ app/Pair.php
@@ -32,7 +32,6 @@ class Pair extends Model
                 );
 
                 $distance = [
-                    "f"     => "arccosine",
                     "m"     => $degrees * 111133.84, // 1 degree = 111133.84 m, based on the average diameter of the Earth (12,735,000 m)
                     "km"    => $degrees * 111.13384, // 1 degree = 111.13384 km, based on the average diameter of the Earth (12,735 km)
                     "mi"    => $degrees * 69.05482 // 1 degree = 69.05482 miles, based on the average diameter of the Earth (7,913.1 miles)
@@ -52,7 +51,6 @@ class Pair extends Model
                 $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
 
                 $distance = [
-                    "f"     => "haversine",
                     "m"     => $earth_radius * $c * 1000, // 1 km = 1000 m
                     "km"    => $earth_radius * $c, // distance in km by default because earch radius was in km
                     "mi"    => $earth_radius * $c * 0.621371192 // 1 km = 0.621371192 mile
